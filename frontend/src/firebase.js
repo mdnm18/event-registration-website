a0c64d26e36ee20e0c2021986fa9ffa94e54823e
mdnm18 <md.n.m.pius18@gmail.com>
2025-09-30 00:14:39 +0530

    update

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,15 +1,13 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuMNFzKyGY4Is11c4eJhFQ6VPZLN-AXYw",
-  authDomain: "srm-virtual-lab-gnu.firebaseapp.com",
-  projectId: "srm-virtual-lab-gnu",
-  storageBucket: "srm-virtual-lab-gnu.firebasestorage.app",
-  messagingSenderId: "211161993480",
-  appId: "1:211161993480:web:27d173458e82fb53bc1cbd",
-  measurementId: "G-QRDH4NWGT0",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
